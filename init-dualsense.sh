e5e746bd3d8e3d693ceb2018aee9f25f45544068
Fabrício Milanez <fabriciomilanez@protonmail.com>
2024-10-13 13:33:37 -0300

    Fix lightbar setup

--- init-dualsense.sh
+++ init-dualsense.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/sh
 
 # lightbar
-dualsensectl lightbar on
-dualsensectl lightbar 255 0 255 255
+dualsensectl lightbar off
 dualsensectl player-leds 0
+dualsensectl lightbar 255 0 255 255
 
 # audio devices
 dualsensectl microphone off
